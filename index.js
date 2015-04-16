1a9fc436e78c0bef3011881c73d820a4e36c9c37
Gerald Leenerts <gerald.leenerts@gmail.com>
2015-04-16 10:07:08 -0600

    RxJS test written. Cleaned up lodash to be more in line.

---
+++ index.js
@@ -0,0 +1,16 @@
+
+console.log('Start Lodash');
+console.log('- - - - - - - - - - - -');
+require('./lodash_test');
+
+console.log('\n\n');
+
+console.log('Start RxJS');
+console.log('- - - - - - - - - - - -');
+require('./rxjs_test');
+
+console.log('\n\n');
+
+console.log('Start Bacon');
+console.log('- - - - - - - - - - - -');
+require('./bacon_test');
--- rxjs_test.js
+++ rxjs_test.js
@@ -1,4 +1,20 @@
 require('./formatNumber');
 var Rx = require('rx');
 
-var meters_array = new Array(10000);
+console.time('RxJS Streaming');
+
+var distance_stream = Rx.Observable.range(0, 10000).map(function(meters) {
+  // Convert to Miles
+  return Math.round(meters / 1609.344 * 100) / 100;
+}).distinct().filter(function (miles) {
+  // Filter to only values on the 1/4 mile.
+  return (miles * 100) % 25 === 0;
+});
+
+var distance_subscription = distance_stream.subscribe(
+    function (miles) {
+      // Format remaining Values...
+      console.log(miles.formatNumber(2, '.',',') + ' Mi');
+    });
+
+console.timeEnd('RxJS Streaming');
